2070d83d5d32f7d2ccb40e4709ecacf5609bb95e
brandoline <heli.01brandao@hotmail.com>
2024-09-22 14:13:23 -0300

    n Commit

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,15 +1,56 @@
 #include <iostream>
 #include <ctype.h>
 #include <stdbool.h>
-#include "Calculos.h"
+#include <locale.h>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+// ADICAO
+double Adicao(double x){
+    double NumeroDois = 0;
+    cin >> NumeroDois;
+
+    return x + NumeroDois;
+}
+
+// SUBTRACAO
+double Subtracao(double x){
+    double NumeroDois = 0;
+    cin >> NumeroDois;
+
+    return x - NumeroDois;
+}
+
+// DIVISAO
+double Divisao(double x){
+    double NumeroDois = 0;
+    cin >> NumeroDois;
+
+    return x / NumeroDois;
+}
+
+// MULTIPLICACAO
+double Multiplicacao(double x){
+    double NumeroDois = 0;
+    cin >> NumeroDois;
+
+    return x * NumeroDois;
+}
+
 int main() {
+    setlocale(LC_ALL, "Portuguese");
     
+    cout << "[BEM-VINDO A  SUA CALCULADORA BÁSICA]" << endl;
+    cout << "\nVOcê terá acesso a operações básicas como adição, subtração, multiplicação e divisão.";
+    cout << "\nPodendo também limpar seu histórico através das teclas C(Clean) e E(Exit)" << endl;
+    cout << "\nPara começar, basta digitar o número inicial e logo após escolher sua operação através dos símbolos +, -, /, *, c, e" << endl;
+
     double NumeroUm = 0, NumeroDois = 0, Resultado = 0;
     char Opcao;
 
-    bool Saida = false;
+    bool Exit = false;
     cin >> NumeroUm;
 
 do{
@@ -51,8 +92,8 @@ do{
         break;  
 
         // SAIDA 
-        case 'S':
-            Saida = true;
+        case 'E':
+            Exit = true;
         break;
 
         default:
@@ -60,7 +101,7 @@ do{
         break;
     }
 
-}while(!Saida);
+}while(!Exit);
 
     return 0;
 }
